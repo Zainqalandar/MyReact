80ce518913344ff1478bf102c25107bf9bff0a64
Zainqalandar <zainqlandar@gmail.com>
2023-09-14 18:29:48 +0500

    Init ContextApi

---
+++ 01ContextAPI/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' text-red-700 text-2xl text-center'>ContextApp</h1>
+    </>
+  )
+}
+
+export default App
