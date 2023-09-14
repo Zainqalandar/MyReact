1fede09e544d5c5fedebc914852445395f9f1057
Zainqalandar <zainqlandar@gmail.com>
2023-09-14 17:59:00 +0500

    border rounded

--- SimpleProjects/src/App.jsx
+++ SimpleProjects/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-800 text-2xl text-center mx-auto bg-yellow-500 p-3 w-1/2 font-bold'>Hello World</h1>
+      <h1 className='text-red-800 text-2xl text-center mx-auto bg-yellow-500 p-3 w-1/2 font-bold my-3 rounded-md border border-red-800'>Hello World</h1>
     </>
   )
 }
