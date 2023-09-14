bda004f0994e0e1b918c5d2747bb864e6bc7e9e5
Zainqalandar <zainqlandar@gmail.com>
2023-09-14 17:51:35 +0500

    This is Init Repository

---
+++ SimpleProjects/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-800 text-2xl text-center mx-auto bg-yellow-500 p-3 w-1/2 font-bold'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
